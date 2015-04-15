bf648ec1fae9277d82187a18ec3654076a768d0e
Kyle Coburn <kylecoburn@gmail.com>
2015-04-15 13:40:24 -0700

    1.4.3

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -- OPTIONS
 
-RESET_FOR_TIME = true -- Set to false if you just want to see the bot finish a run
+RESET_FOR_TIME = false -- Set to false if you just want to see the bot finish a run
 
 local CUSTOM_SEED = nil -- Set to a known seed to replay it, or leave nil for random runs
 local NIDORAN_NAME = "A" -- Set this to the single character to name Nidoran (note, to replay a seed, it MUST match!)
@@ -8,7 +8,7 @@ local PAINT_ON    = true -- Display contextual information while the bot runs
 
 -- START CODE (hard hats on)
 
-VERSION = "1.4.2"
+VERSION = "1.4.3"
 YELLOW = memory.getcurrentmemorydomainsize() > 30000
 GAME_NAME = YELLOW and "yellow" or "red"
 
